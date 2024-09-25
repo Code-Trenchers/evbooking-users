6d34b7aa7fc55fbd29a271452ee96396ec75b0d8
Udhayaa V D <udhayaaudhayaa987@gmail.com>
2024-09-25 14:50:19 +0530

    Setup Components in home page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,23 +2,55 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:login_page/screens/login_screen.dart';
-import 'package:login_page/widgets/error_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
   @override
   HomeScreenState createState() => HomeScreenState();
 }
 
 class HomeScreenState extends State<HomeScreen> {
   final String serverKey = "";
+  User? _user;
+
+  // Variables to store dropdown selections
+  String? _selectedLocation;
+  String? _selectedDestination;
+  String? _selectedDesignation;
+  String? _selectedLuggageStatus;
+  String? _selectedPurpose;
+  String? _otherPurposeText;
+
+  final List<String> _locations = [
+    'Boys Hostel',
+    'Girls Hostel',
+    'Sunflower Block',
+    'Mechanical Block',
+    'As Block',
+    'IB Block',
+    'Cafeteria',
+    'BIT Main Auditorium',
+    'Aero Block',
+    'Gate A',
+    'Learning Centre',
+    'Medical Centre',
+    'Gate C',
+    'Badminton Court',
+    'Main Ground'
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _user = FirebaseAuth.instance.currentUser;
+  }
 
   Future<void> sendMessage() async {
     const postUrl = 'https://fcm.googleapis.com/fcm/send';
     final data = {
-      "to": "RECEIVER_DEVICE_TOKEN", // Replace with receiver's FCM token
+      "to": "RECEIVER_DEVICE_TOKEN",
       "notification": {
         "title": "Message from App 1",
         "body": "Hello from another app!",
@@ -48,11 +80,10 @@ class HomeScreenState extends State<HomeScreen> {
     }
   }
 
-  /// Signs the user out and navigates to the login screen
   Future<void> signUserOut() async {
     await FirebaseAuth.instance.signOut();
 
-    if (!mounted) return; // Check if the widget is still mounted
+    if (!mounted) return;
 
     Navigator.pushAndRemoveUntil(
       context,
@@ -61,189 +92,255 @@ class HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void buttonAction() {
-    try {
-      print("Hello");
-    } catch (e) {
-      print(e);
-    }
-  }
-
-  /// Navigate to Profile screen (placeholder)
-  void navigateToProfile() {
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Navigating to Profile...'),
-    ));
-  }
-
-  /// Navigate to Settings screen (placeholder)
-  void navigateToSettings() {
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Navigating to Settings...'),
-    ));
-  }
-
-  /// Navigate to About screen (placeholder)
-  void navigateToAbout() {
-    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-      content: Text('Navigating to About...'),
-    ));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Home'),
-        backgroundColor: Colors.grey, // Set AppBar color to grey
+        backgroundColor: Colors.deepPurple,
+        foregroundColor: Colors.white,
       ),
       drawer: Drawer(
         child: Column(
           children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Theme.of(context).primaryColor,
+            UserAccountsDrawerHeader(
+              decoration: const BoxDecoration(
+                gradient:
+                    LinearGradient(colors: [Colors.deepPurple, Colors.purple]),
               ),
-              child: Text(
-                'Welcome, ${_user.displayName!}',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
+              accountName: Text(_user?.displayName ?? ''),
+              accountEmail: Text(_user?.email ?? ''),
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                backgroundImage: _user?.photoURL != null
+                    ? NetworkImage(_user!.photoURL!)
+                    : null,
+                child: _user?.photoURL == null
+                    ? Text(
+                        _user?.displayName?.substring(0, 1) ?? 'G',
+                        style: const TextStyle(fontSize: 40.0),
+                      )
+                    : null,
               ),
             ),
-            ListTile(
-              leading: const Icon(Icons.person),
-              title: const Text('Profile'),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-                navigateToProfile(); // Navigate to Profile
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.settings),
-              title: const Text('Settings'),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-                navigateToSettings(); // Navigate to Settings
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.info),
-              title: const Text('About'),
-              onTap: () {
-                Navigator.pop(context); // Close the drawer
-                navigateToAbout(); // Navigate to About
-              },
-            ),
-            const SizedBox(height: 420), // Add space before Logout
             ListTile(
               leading: const Icon(Icons.logout),
               title: const Text('Logout'),
               onTap: () {
-                Navigator.pop(context); // Close the drawer
-                signUserOut(); // Call the sign-out function
+                Navigator.pop(context);
+                signUserOut();
               },
             ),
           ],
         ),
       ),
-      body: Padding(
-        padding:
-            const EdgeInsets.all(16.0), // Add some padding around the content
-        child: Wrap(
-          spacing: 20.0, // Horizontal spacing between items
-          runSpacing: 20.0, // Vertical spacing between rows
-          children: [
-            // Box 1 with grey and mild black fade background
-            SizedBox(
-              width: (MediaQuery.of(context).size.width / 2) -
-                  30, // Half of the screen width
-              child: Container(
-                height: 300,
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [Colors.grey, Colors.black54],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                  borderRadius: BorderRadius.circular(15), // Rounded corners
-                ),
-                child: const Center(
-                  child: Text(
-                    '...',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                ),
-              ),
-            ),
-            // Box 2 with grey and mild black fade background
-            SizedBox(
-              width: (MediaQuery.of(context).size.width / 2) -
-                  30, // Half of the screen width
-              child: Container(
-                height: 300,
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [Colors.grey, Colors.black54],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.white, Colors.purpleAccent],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(15),
                 ),
-                child: const Center(
-                  child: Text(
-                    '...',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Book Your Slot',
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.deepPurple,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+
+                      // Dropdown for Current Location
+                      DropdownButtonFormField<String>(
+                        value: _selectedLocation,
+                        decoration: InputDecoration(
+                          labelText: 'Current Location',
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        items: _locations.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedLocation = newValue;
+                            _selectedDestination = null;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 20),
+
+                      // Dropdown for Destination Location
+                      DropdownButtonFormField<String>(
+                        value: _selectedDestination,
+                        decoration: InputDecoration(
+                          labelText: 'Destination Location',
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        items: _locations
+                            .where((location) => location != _selectedLocation)
+                            .map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedDestination = newValue;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 20),
+
+                      // Designation
+                      DropdownButtonFormField<String>(
+                        value: _selectedDesignation,
+                        decoration: InputDecoration(
+                          labelText: 'Designation',
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        items: <String>['Student', 'Faculty', 'Technician']
+                            .map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedDesignation = newValue;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 20),
+
+                      // Luggage Status
+                      DropdownButtonFormField<String>(
+                        value: _selectedLuggageStatus,
+                        decoration: InputDecoration(
+                          labelText: 'Luggage Available Status',
+                          filled: true,
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                        items: <String>['Yes', 'No'].map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (newValue) {
+                          setState(() {
+                            _selectedLuggageStatus = newValue;
+                          });
+                        },
+                      ),
+                      const SizedBox(height: 20),
+
+                      if (_selectedLuggageStatus == 'No') ...[
+                        // Purpose Dropdown
+                        DropdownButtonFormField<String>(
+                          value: _selectedPurpose,
+                          decoration: InputDecoration(
+                            labelText: 'Purpose',
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          items: <String>['Meeting', '.', '.', 'Other']
+                              .map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          onChanged: (newValue) {
+                            setState(() {
+                              _selectedPurpose = newValue;
+                              _otherPurposeText = null;
+                            });
+                          },
+                        ),
+                        const SizedBox(height: 20),
+
+                        // Other Purpose Text Field
+                        if (_selectedPurpose == 'Other') ...[
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: 'Specify',
+                              filled: true,
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                            onChanged: (text) {
+                              setState(() {
+                                _otherPurposeText = text;
+                              });
+                            },
+                          ),
+                          const SizedBox(height: 20),
+                        ],
+                      ],
+
+                      // Submit Button with Ripple Effect
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 40, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          backgroundColor: Colors.deepPurple,
+                          foregroundColor: Colors.white, // Correct parameter
+                        ),
+                        onPressed: () {
+                          // Handle form submission here
+                        },
+                        child: const Text('Submit'),
+                      ),
+                    ],
                   ),
                 ),
               ),
             ),
-            // Box 3 with grey and mild black fade background
-            SizedBox(
-              width: (MediaQuery.of(context).size.width / 2) -
-                  30, // Half of the screen width
-              child: Container(
-                height: 300,
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [Colors.grey, Colors.black54],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: const Center(
-                  child: Text(
-                    '...',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                ),
-              ),
-            ),
-            // Box 4 with grey and mild black fade background
-            SizedBox(
-              width: (MediaQuery.of(context).size.width / 2) -
-                  30, // Half of the screen width
-              child: Container(
-                height: 300,
-                decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                    colors: [Colors.grey, Colors.black54],
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                  ),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: const Center(
-                  child: Text(
-                    '...',
-                    style: TextStyle(color: Colors.white, fontSize: 18),
-                  ),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
